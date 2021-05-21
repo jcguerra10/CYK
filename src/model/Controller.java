2c1b6df087c1f55236b92d567f9d8dcb3d85f7af
jcguerra10 <juancamiloguerratabares@gmail.com>
2021-05-21 18:01:40 -0500

    cyk method and control the warnings

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -2,20 +2,17 @@ package model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Controller {
-    private String path;
-    private String w;
-    private String initS;
+    private final String path;
+    private final String w;
+    private final String initS;
 
-    private ArrayList<String> terminals = new ArrayList<>();
-    private ArrayList<String> nonTerminals = new ArrayList<>();
+    private final ArrayList<String> terminals = new ArrayList<>();
+    private final ArrayList<String> nonTerminals = new ArrayList<>();
 
-    private HashMap<String, ArrayList<String>> g = new HashMap<>();
+    private final HashMap<String, ArrayList<String>> g = new HashMap<>();
 
     public Controller(String path, String w, String initS) {
         this.path = path;
@@ -23,23 +20,22 @@ public class Controller {
         this.initS = initS;
     }
 
+    @SuppressWarnings({"rawtypes", "unchecked"})
     public void setupGrammar() throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(path));
-        ArrayList<String> tmp = new ArrayList<>();
 
-        tmp.addAll(Arrays.asList(toArray(scanner.nextLine())));
+        ArrayList<String> tmp = new ArrayList<>(Arrays.asList(toArray(scanner.nextLine())));
         terminals.addAll(tmp);
         tmp.clear();
         tmp.addAll(Arrays.asList(toArray(scanner.nextLine())));
         nonTerminals.addAll(tmp);
         tmp.clear();
 
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             tmp.addAll(Arrays.asList(toArray(scanner.nextLine())));
             String l = tmp.get(0);
             tmp.remove(0);
-            ArrayList arr = new ArrayList();
-            arr.addAll(tmp);
+            ArrayList arr = new ArrayList(tmp);
             g.put(l, arr);
             tmp.clear();
         }
@@ -57,11 +53,79 @@ public class Controller {
         for (int i = 1; i < table.length; i++) {
             table[i] = new String[length - (i - 1)];
         }
-        for (int i = 1;i < table.length;i++) {
-            for (int j = 0; j < table[i].length; j++) {
-                table[i][j] = "";
+        for (int i = 1; i < table.length; i++) {
+            Arrays.fill(table[i], "");
+        }
+        return table;
+    }
+
+    public String[][] cyk(String[][] table) {
+        for (int i = 0; i < table[0].length; i++) {
+            table[0][i] = w.charAt(i) + "";
+        }
+        for (int i = 0; i < table[i].length; i++) {
+            String[] comb = productions(new String[]{table[0][i]});
+            table[1][i] = Arrays.toString(comb).replaceAll("[\\[\\],]", "");
+        }
+        if (w.length() >= 1) {
+            for (int i = 0; i < table[2].length; i++) {
+                String[] down = toArray(table[1][i]);
+                String[] diag = toArray(table[1][i + 1]);
+                String[] aCombs = getAllCombs(down, diag);
+                String[] comb = productions(aCombs);
+                table[2][i] = Arrays.toString(comb).replaceAll("[\\[\\],]", "");
+            }
+            if (w.length() >= 2){
+                restOps(table);
             }
         }
         return table;
     }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void restOps(String[][] table) {
+        for (int i = 3; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                for (int k = 0; k < i; k++) {
+                    String[] down = table[k][i].split("\\s");
+                    String[] diag = table[i-k][j+k].split("\\s");
+                    String[] aCombs = getAllCombs(down, diag);
+                    String[] comb = productions(aCombs);
+                    if (table[i][j].isEmpty()) {
+                        table[i][j] = Arrays.toString(comb).replaceAll("[\\[\\],]", "");
+                    } else {
+                        ArrayList newVal = (ArrayList) Arrays.asList(toArray(table[i][j]));
+                        newVal.addAll(Arrays.asList(comb));
+                        HashSet<String> currentV = new HashSet<>(newVal);
+                        String[] res = currentV.toArray(new String[0]);
+                        table[i][j] = Arrays.toString(res).replaceAll("[\\[\\],]", "");
+                    }
+                }
+            }
+        }
+    }
+
+    private String[] getAllCombs(String[] down, String[] diag) {
+        int n = down.length * diag.length;
+        String[] combs = new String[n];
+        for (String s : down) {
+            for (int j = 0; j < diag.length; j++) {
+                combs[j] = s + diag[j];
+            }
+        }
+        return combs;
+    }
+
+    private String[] productions(String[] strings) {
+        ArrayList<String> tmp = new ArrayList<>();
+        for (String s : g.keySet()) {
+            for (String tc : strings) {
+                if (g.get(s).contains(tc)) {
+                    tmp.add(s);
+                }
+            }
+        }
+        return tmp.toArray(new String[0]);
+    }
+
 }
